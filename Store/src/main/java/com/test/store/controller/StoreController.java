f7a827f941597afbdb644a513948eff8bfe94582
mkerko <kerkomikhail@gmail.com>
2018-12-10 11:19:58 +0300

    Fixed missing imports

--- Store/src/main/java/com/test/store/controller/StoreController.java
+++ Store/src/main/java/com/test/store/controller/StoreController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
